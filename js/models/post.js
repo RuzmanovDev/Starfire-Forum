2fc42af8448b142edbb15128928e0a878d384489
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-09-27 14:55:26 +0300

    Added id generator for posts

--- js/models/post.js
+++ js/models/post.js
@@ -1,5 +1,13 @@
+const idGenerator = function *() {
+    let index = 1;
+    while (true) {
+        yield index++;
+    }
+}();
+
 class Post {
     constructor(author, content) {
+        this._id = idGenerator.next().value;
         this.author = author;
         this.content = content;
         this.date = new Date();
