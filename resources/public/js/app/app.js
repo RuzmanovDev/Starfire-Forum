4729a95ea91e9365e5f2c70f5f60c8e7d7d01f67
Ivan Bozhkov <bozhkov.code@gmail.com>
2016-09-30 04:03:16 +0300

    add navigations & apply styles on index

--- resources/public/js/app/app.js
+++ resources/public/js/app/app.js
@@ -31,3 +31,48 @@ const sammyApp = Sammy(wrapper, function () {
 });
 
 sammyApp.run('#/');
+
+$(function(){
+
+    /// да, да.. знам, че не трябва да е тука
+    /// ще го махна по някое време
+    ///
+
+    $(".navigation").each(function(){
+        calcIndicatorWidth.call(this);
+    })
+    function calcIndicatorWidth() {
+        let indicator   = $(this).find(".activity-indicator"),
+            active      = $(this).find(".active").length ? $(this).find(".active") : $(this).find(" > a").eq(0).addClass("active"),
+            activeIndex = $(this).attr("activeIndex") || 0;
+
+
+        if(activeIndex < active.index()) {
+            indicator.addClass("to-right").removeClass("to-left");
+        }
+        else {
+            indicator.addClass("to-left").removeClass("to-right");
+        }
+
+        indicator.css({
+            "margin-left"  : active.position().left,
+            "margin-right" : $(this).width() - (active.outerWidth() + active.position().left)
+        })
+        $(this).attr("activeIndex", active.index());
+    }
+
+    $(document).on("click", ".navigation", calcIndicatorWidth);
+
+    $(document).on("click", ".navigation > a", function(e) {
+        $(this).addClass("active")
+            .siblings().removeClass("active");
+
+        $(this).parent().trigger("click");
+    });
+
+    $(window).resize(function() {
+        $(".navigation").each(function(){
+            calcIndicatorWidth.call(this);
+        })
+    })
+})
