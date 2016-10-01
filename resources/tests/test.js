ec82bca5070ffaa3c99bff4a44e89aef18cd6591
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-10-01 15:14:35 +0300

    Fixed routes

--- resources/tests/test.js
+++ resources/tests/test.js
@@ -1,4 +1,7 @@
 /* globals  describe it */
+import {requester} from 'requester'
+import {userData} from 'user-data'
+
 mocha.setup('bdd');
 
 const {expect, assert} = chai;
@@ -10,10 +13,31 @@ const user = {
 };
 describe('Login tests', function () {
 
-    it('asdas', function () {
-        expect(2).to.eql(2);
-    })
+    beforeEach(function() {
+        sinon.stub(requester, 'post')
+            .returns(new Promise((resolve, reject) => {
+                resolve({
+                    result: {
+                        username: user.username,
+                        authKey: AUTH_KEY
+                    }
+                });
+            }));
+        localStorage.clear();
+    });
 
+    afterEach(function() {
+        requester.post.restore();
+        localStorage.clear();
+    });
+
+    it('expect post to be called once', function(done) {
+        userData.login(user)
+            .then(() => {
+                expect(requester.post.calledOnce).to.be.true;
+            })
+            .then(done, done);
+    });
 });
 
 
